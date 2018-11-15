d927718b8cd49cf9114e89f0def1242a56162368
kvswamy <32405387+kvswamy@users.noreply.github.com>
2018-11-15 16:51:29 +0800

    commite test

--- src/main/java/com/example/devops/web/WelcomeController.java
+++ src/main/java/com/example/devops/web/WelcomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class WelcomeController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
